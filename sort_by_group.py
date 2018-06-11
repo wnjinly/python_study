05b54591acf43c6224c17f34ce343a3d50949b8d
wnjinly <wnjinly@163.com>
2018-06-11 16:54:51 +0800

    sort by group

---
+++ sort_by_group.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+
+from operator import itemgetter
+from itertools import groupby
+from collections import defaultdict
+# 对下面数据按照，date进行分组
+rows = [
+    {'address': '5412 N CLARK', 'date': '07/01/2012'},
+    {'address': '5148 N CLARK', 'date': '07/04/2012'},
+    {'address': '5800 E 58TH', 'date': '07/02/2012'},
+    {'address': '2122 N CLARK', 'date': '07/03/2012'},
+    {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+    {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+    {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+    {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
+
+# 使用itertools.groupby()
+# 使用groupby()首先需要进行排序，否则会出现各种未知问题
+
+rows.sort(key=itemgetter('date'))
+for date, items in groupby(rows, key=itemgetter('date')):
+    print(date)
+    for i in items:
+        print(' ', i)
+
+
+# 使用defaultdict
+
+rows_by_date = defaultdict(list)
+for row in rows:
+    rows_by_date[row['date']].append(row)
+
+for i in rows_by_date:
+    for j in rows_by_date[i]:
+    	print(j)
+
+print(rows_by_date)
